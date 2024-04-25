cfac8b7da16aa3177271554cb54b3ecca13837ae
roberto <tkp1@hanmail.net>
2024-04-26 04:36:57 +0900

    [feat] : 더보기 로직 수정

--- src/hooks/use-coin-fetch.ts
+++ src/hooks/use-coin-fetch.ts
@@ -8,11 +8,19 @@ import { getBookmarkFromLocalStorage } from '../utils/local-storage-utils'
 export const useCoinFetch = ({ params, viewOptions }: { params: CoinsParams; viewOptions: string }) => {
   const [coinData, setCoinData] = useState<Coin[]>([])
 
-  const { data, error } = useQuery<Coin[], Error>(['coins', params], () => fetchCoins(params))
+  const { data, error } = useQuery<Coin[], Error>(['coins', params], () => fetchCoins(params), {
+    keepPreviousData: true, // 이전 데이터 유지
+    getNextPageParam: (lastPage, allPages) => allPages.length + 1, // 페이지네이션을 위한 설정
+  })
+
+  // 새 데이터가 로드될 때마다 기존 데이터에 추가
+  useEffect(() => {
+    if (data) {
+      setCoinData((prev) => [...prev, ...data])
+    }
+  }, [data])
 
   useEffect(() => {
-    setCoinData(data ?? [])
-
     if (viewOptions === 'bookMark') {
       const bookmarkedCoinsData = getBookmarkFromLocalStorage()
       if (bookmarkedCoinsData) return setCoinData(bookmarkedCoinsData)
